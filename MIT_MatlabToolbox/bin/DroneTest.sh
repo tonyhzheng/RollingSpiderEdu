7b35eb7c9a182678c01850bf542548a56a91e2c1
student <tony.h.zheng@gmail.com>
2018-03-01 02:58:32 -0800

    slow lqr success

--- MIT_MatlabToolbox/bin/DroneTest.sh
+++ MIT_MatlabToolbox/bin/DroneTest.sh
@@ -10,7 +10,7 @@ cd `dirname $0`
 set timeout -1;
 spawn telnet 192.168.1.1;
 expect "RS.edu] \$ ";
-send "sed -i \"s/POWERGAIN : \[0-9\]*;/POWERGAIN : 0;/\" /data/edu/params/paramsEDU.dat\r";
+send "sed -i \"s/POWERGAIN : \[1-9\]\[0-9\]*;/POWERGAIN : 10;/\" /data/edu/params/paramsEDU.dat\r";
 
 expect "RS.edu] \$ ";
 send "sed -i \"s/FEAT_NOSAFETY : \[0-1\];/FEAT_NOSAFETY : 1;/\" /data/edu/params/paramsEDU.dat\r";
