39f665c597e191ba8808d733cda56d5c1e6a9a43
student <tony.h.zheng@gmail.com>
2018-02-28 20:40:55 -0800

    x

--- MIT_MatlabToolbox/bin/DroneTest.sh
+++ MIT_MatlabToolbox/bin/DroneTest.sh
@@ -4,12 +4,19 @@
 cd `dirname $0`
 
 # 2. Telnet into drone, adjust PowerGain in paramsEDU.dat and start the SpiderFlight.sh
+# send "sed -i \"s/POWERGAIN : \[1-9\]\[0-9\]*;/POWERGAIN : 10;/\" /data/edu/params/paramsEDU.dat\r";
+# send "sed -i \"s/POWERGAIN : \[0-9\]*;/POWERGAIN : 0;/\" /data/edu/params/paramsEDU.dat\r";
 /usr/bin/expect <<SCRIPT
 set timeout -1;
 spawn telnet 192.168.1.1;
 expect "RS.edu] \$ ";
-send "sed -i \"s/POWERGAIN : \[1-9\]\[0-9\]*;/POWERGAIN : 10;/\" /data/edu/params/paramsEDU.dat\r";
-expect "RS.edu] \$ ";send "killall dragon-prog\r"; expect "RS.edu] \$ ";
+send "sed -i \"s/POWERGAIN : \[0-9\]*;/POWERGAIN : 0;/\" /data/edu/params/paramsEDU.dat\r";
+
+expect "RS.edu] \$ ";
+send "sed -i \"s/FEAT_NOSAFETY : \[0-1\];/FEAT_NOSAFETY : 1;/\" /data/edu/params/paramsEDU.dat\r";
+
+expect "RS.edu] \$ ";
+send "killall dragon-prog\r"; expect "RS.edu] \$ ";
 send "SpiderFlight.sh\r";
 expect "RS.edu] \$ ";
 send "exit\r";
